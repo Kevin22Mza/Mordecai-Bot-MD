b5a12971487ef5f7c6024c31649a17eaa06efda5
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-22 09:44:32 -0300

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -17,7 +17,7 @@ let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para con
 - ${namechannel}
 *❀* ${channel}
 
-- ✧┊┋◟𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮 𝐓𝐞𝐬𝐭◞┊┋✧
+- ✧┊┋◟𝙈𝙤𝙧𝙙𝙚𝙘𝙖𝙞 𝘽𝙤𝙩 𝙏𝙚𝙨𝙩◞┊┋✧
 *❀* ${channel2}
 
 > ${dev}`
