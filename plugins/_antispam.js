5df03e94a3f58d070cd28deec6bd113f7395205b
Kevin22Mza <167890463+Kevin22Mza@users.noreply.github.com>
2025-01-21 21:34:59 -0300

    Actualizar _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -35,19 +35,19 @@ const timeDifference = currentTime - userData.lastMessageTime
 if (userData.antiBan === 1) {
 if (userData.message < 1) {
 userData.message++  
-motive = `🍭 𝙉𝙤 𝙝𝙖𝙜𝙖𝙨 𝙨𝙥𝙖𝙢.`
+motive = `🍭 𝙉𝙤 𝙃𝙖𝙜𝙖𝙨 𝙎𝙥𝙖𝙢.`
 await conn.reply(m.chat, motive, m, { mentions: [m.sender] })  
 user.messageSpam = motive
 }} else if (userData.antiBan === 2) {
 if (userData.message2 < 1) {
 userData.message2++  
-motive =  `🍭 𝙉𝙤 𝙝𝙖𝙜𝙖𝙨 𝙨𝙥𝙖𝙢...`
+motive =  `🍭 𝙉𝙤 𝙃𝙖𝙜𝙖𝙨 𝙎𝙥𝙖𝙢...`
 await conn.reply(m.chat, motive, m, { mentions: [m.sender] })  
 user.messageSpam = motive
 }} else if (userData.antiBan === 3) {
 if (userData.message3 < 1) {
 userData.message3++  
-motive = `🍭 𝙎𝙚𝙧𝙖𝙨 𝙚𝙡𝙞𝙢𝙞𝙣𝙖𝙙𝙤(𝙖) 𝙥𝙤𝙧 𝙝𝙖𝙘𝙚𝙧 𝙨𝙥𝙖𝙢.`
+motive = `🍭 𝙎𝙚𝙧𝙖𝙨 𝙀𝙡𝙞𝙢𝙞𝙣𝙖𝙙𝙤(𝙖) 𝙥𝙤𝙧 𝙝𝙖𝙘𝙚𝙧 𝙎𝙥𝙖𝙢.`
 await conn.reply(m.chat, motive, m, { mentions: [m.sender] }) 
 user.messageSpam = motive
 await conn.groupParticipantsUpdate(m.chat, [sender], 'remove')
